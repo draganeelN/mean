1757c74a77efe29ac96a013f76e5a66e2fa6c303
draganeelN <44310702+draganeelN@users.noreply.github.com>
2020-08-19 15:54:42 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+const Joi = //require('joi');
 
 // require and configure dotenv, will load vars in .env in PROCESS.ENV
 require('dotenv').config();
@@ -6,12 +6,12 @@ require('dotenv').config();
 // define validation for all the env vars
 const envVarsSchema = Joi.object({
   NODE_ENV: Joi.string()
-    .allow(['development', 'production', 'test', 'provision'])
+    .allow//(['development', 'production', 'test', 'provision'])
     .default('development'),
   SERVER_PORT: Joi.number()
     .default(4040),
   MONGOOSE_DEBUG: Joi.boolean()
-    .when('NODE_ENV', {
+    .when(//'NODE_ENV', {
       is: Joi.string().equal('development'),
       then: Joi.boolean().default(true),
       otherwise: Joi.boolean().default(false)
